79fb18280b1e373bfccdaaa39ffe0ecc861babc2
bpedazur <bpedazur@gmail.com>
2020-05-27 16:07:51 -0400

    removed api-key

---
+++ Interior Design Demo/Assets/Interior Design/ResetScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * Resets the scene by loading it again
+ */
+public class ResetScript : MonoBehaviour
+{
+    public void ResetScene()
+    {
+        SceneManager.LoadScene("InteriorDesign");
+    }
+}
